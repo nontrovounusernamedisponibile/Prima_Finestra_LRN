7cecf0c44b5164abd01cd91851b20e753fdd4927
nontrovounusernamedisponibile <cerasuoloantonio2004@gmail.com>
2025-11-23 00:38:11 +0100

    Enciclopedia delle varie mesagebox V1 (forse ultima versione forse no)

---
+++ MessageBoxEncyclopedya.cpp
@@ -0,0 +1,92 @@
+#include <windows.h>
+
+int WINAPI wWinMain(HINSTANCE hInst, HINSTANCE hPrevHinst, LPWSTR args, int nCmdShow){
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_ABORTRETRYIGNORE" , MB_ABORTRETRYIGNORE);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_APPLMODAL" , MB_APPLMODAL);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_CANCELTRYCONTINUE" , MB_CANCELTRYCONTINUE);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_COMPOSITE" , MB_COMPOSITE);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_CUR_MAX" , MB_CUR_MAX);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_DEFAULT_DESKTOP_ONLY" , MB_DEFAULT_DESKTOP_ONLY);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_DEFBUTTON1" , MB_DEFBUTTON1);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_DEFBUTTON2" , MB_DEFBUTTON2);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_DEFBUTTON3" , MB_DEFBUTTON3);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_DEFBUTTON4" , MB_DEFBUTTON4);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_DEFMASK" , MB_DEFMASK);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_ERR_INVALID_CHARS" , MB_ERR_INVALID_CHARS);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_HELP" , MB_HELP);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_ICONASTERISK" , MB_ICONASTERISK);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_ICONERROR" , MB_ICONERROR);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_ICONEXCLAMATION" , MB_ICONEXCLAMATION);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_ICONHAND" , MB_ICONHAND);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_ICONINFORMATION" , MB_ICONINFORMATION);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_ICONMASK" , MB_ICONMASK);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_ICONQUESTION" , MB_ICONQUESTION);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_ICONSTOP" , MB_ICONSTOP);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_ICONWARNING" , MB_ICONWARNING);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_LEN_MAX" , MB_LEN_MAX);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_MISCMASK" , MB_MISCMASK);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_MODEMASK" , MB_MODEMASK);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_NOFOCUS" , MB_NOFOCUS);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_OK" , MB_OK);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_OKCANCEL" , MB_OKCANCEL);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_PRECOMPOSED" , MB_PRECOMPOSED);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_RETRYCANCEL" , MB_RETRYCANCEL);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_RIGHT" , MB_RIGHT);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_RTLREADING" , MB_RTLREADING);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_SERVICE_NOTIFICATION" , MB_SERVICE_NOTIFICATION);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_SERVICE_NOTIFICATION_NT3X" , MB_SERVICE_NOTIFICATION_NT3X);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_SETFOREGROUND" , MB_SETFOREGROUND);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_SYSTEMMODAL" , MB_SYSTEMMODAL);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_TASKMODAL" , MB_TASKMODAL);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_TOPMOST" , MB_TOPMOST);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_TYPEMASK" , MB_TYPEMASK);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_USEGLYPHCHARS" , MB_USEGLYPHCHARS);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_USERICON" , MB_USERICON);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_YESNO" , MB_YESNO);
+
+    MessageBoxW(NULL, L"Hello World" , L"MB_YESNOCANCEL" , MB_YESNOCANCEL);
+
+    return 0;
+}
